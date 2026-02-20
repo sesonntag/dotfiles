c28c19f8a8a63b13cf4df13a14e5d55ffbe9c27a
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2026-02-21 00:55:11 +0100

    add lua version of my vim config for neovim

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- ============================================================
--- Neovim Configuration
+-- Neovim 2026 Fast Config
 -- Author: Sebastian Sonntag
 -- License: MIT
 -- ============================================================
@@ -17,9 +17,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- ============================================================
--- Leader
--- ============================================================
 vim.g.mapleader = " "
 
 -- ============================================================
@@ -27,90 +24,142 @@ vim.g.mapleader = " "
 -- ============================================================
 require("lazy").setup({
 
-  { "scrooloose/nerdtree", cmd = "NERDTreeToggle" },
-  { "ctrlpvim/ctrlp.vim" },
-  { "itchyny/lightline.vim" },
+  -- Colorscheme
   { "joshdick/onedark.vim" },
 
+  -- File tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = true
+  },
+
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = { theme = "onedark" }
+      })
+    end
+  },
+
+  -- Git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end
+  },
+
   { "tpope/vim-fugitive" },
-  { "mhinz/vim-signify" },
-  { "junegunn/gv.vim" },
-  { "gregsexton/gitv" },
 
-  { "tpope/vim-surround" },
-  { "tpope/vim-repeat" },
-  { "tpope/vim-eunuch" },
-  { "jiangmiao/auto-pairs" },
-  { "ntpeters/vim-better-whitespace" },
-  { "machakann/vim-highlightedyank" },
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup()
+    end
+  },
 
-  { "w0rp/ale" },
-  { "pseewald/vim-anyfold" },
-  { "inside/vim-search-pulse" },
-  { "google/vim-searchindex" },
-  { "Yggdroot/indentLine" },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "bash", "yaml", "json", "markdown", "dockerfile"
+        },
+        highlight = { enable = true },
+        indent = { enable = true }
+      })
+    end
+  },
 
-  { "lervag/vimtex", ft = "latex" },
-  { "janko-m/vim-test" },
-  { "mhinz/vim-startify" },
-  { "majutsushi/tagbar", cmd = "TagbarToggle" },
+  -- Indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {}
+  },
+
+  -- Auto pairs (lightweight)
+  { "echasnovski/mini.pairs", version = false, config = true },
+
+  -- Commenting
+  { "numToStr/Comment.nvim", config = true },
+
+  -- Formatting
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          yaml = { "prettier" },
+          json = { "prettier" },
+          markdown = { "prettier" }
+        }
+      })
+    end
+  },
 
 })
 
 -- ============================================================
 -- Options
 -- ============================================================
-vim.opt.history = 1000
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.numberwidth = 5
-vim.opt.cursorline = true
-vim.opt.scrolloff = 5
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.autoread = true
-vim.opt.mouse = "a"
-vim.opt.encoding = "utf-8"
-vim.opt.fileformats = { "unix", "dos", "mac" }
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.completeopt = { "longest", "menuone" }
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.showmatch = true
-vim.opt.matchtime = 2
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "81"
-vim.opt.undofile = true
-vim.opt.undodir = vim.fn.expand("~/.vim/undo//")
-vim.opt.foldlevel = 1
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.scrolloff = 5
+opt.splitbelow = true
+opt.splitright = true
+opt.mouse = "a"
+opt.clipboard:append("unnamedplus")
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+opt.ignorecase = true
+opt.smartcase = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+opt.undofile = true
+opt.colorcolumn = "81"
 
 vim.cmd.colorscheme("onedark")
 
 -- ============================================================
 -- Autocommands
 -- ============================================================
+
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Reload config on save
 autocmd("BufWritePost", {
   pattern = "init.lua",
   command = "source <afile>"
 })
 
+-- Spell in markdown / text
 autocmd("FileType", {
-  pattern = { "markdown", "latex", "text" },
+  pattern = { "markdown", "text" },
   callback = function()
     vim.opt_local.spell = true
   end
 })
 
+-- Strip trailing whitespace
 autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
@@ -120,90 +169,45 @@ autocmd("BufWritePre", {
   end
 })
 
-autocmd("FileType", {
-  pattern = "*",
-  command = "AnyFoldActivate"
-})
+-- ============================================================
+-- Keymaps (Deine gewohnte UX)
+-- ============================================================
 
--- ============================================================
--- Keymaps
--- ============================================================
 local map = vim.keymap.set
 
 -- Save / Quit
-map("n", "<leader>w", ":w!<CR>")
-map("n", "<leader>q", ":q!<CR>")
-map("n", "<leader>x", ":x<CR>")
+map("n", "<leader>w", "<cmd>w!<CR>")
+map("n", "<leader>q", "<cmd>q!<CR>")
+map("n", "<leader>x", "<cmd>x<CR>")
 
 -- Clear search
-map("n", "_", ":nohl<CR>")
+map("n", "_", "<cmd>nohl<CR>")
 
--- Buffer navigation
-map("n", "<leader>bn", ":bn<CR>")
-map("n", "<leader>bN", ":bp<CR>")
-map("n", "<leader>k", ":bd<CR>")
-map("n", "<leader>K", ":bd!<CR>")
+-- File tree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
 
--- NERDTree
-map("n", "<C-n>", ":NERDTreeToggle<CR>")
+-- Telescope
+map("n", "<C-p>", "<cmd>Telescope find_files<CR>")
+map("n", "<C-b>", "<cmd>Telescope buffers<CR>")
+map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>")
 
--- Tagbar
-map("n", "<leader>ct", ":TagbarToggle<CR>")
-
--- Tests
-map("n", "<leader>t", ":TestNearest<CR>")
-map("n", "<leader>T", ":TestFile<CR>")
-map("n", "<leader>a", ":TestSuite<CR>")
-map("n", "<leader>l", ":TestLast<CR>")
-map("n", "<leader>g", ":TestVisit<CR>")
-
--- Insert escape
-map("i", "kj", "<Esc>")
-map("i", ",,", "<Esc>")
-
--- Terminal escape
-map("t", "<Esc>", [[<C-\><C-n>]])
+-- Buffers
+map("n", "<leader>bn", "<cmd>bnext<CR>")
+map("n", "<leader>bN", "<cmd>bprevious<CR>")
+map("n", "<leader>k", "<cmd>bd<CR>")
 
 -- Toggle spell
 map("n", "<leader>s", function()
   vim.opt_local.spell = not vim.opt_local.spell:get()
-  print(vim.opt_local.spell:get() and "Spell ON" or "Spell OFF")
 end)
 
--- ============================================================
--- Plugin Settings
--- ============================================================
-
--- ALE
-vim.g.ale_sign_column_always = 1
-vim.g.ale_sign_error = ">>"
-vim.g.ale_sign_warning = "--"
-vim.g.ale_linters = {
-  python = { "flake8" },
-}
-vim.g.ale_python_flake8_executable = "python3"
-vim.g.ale_python_flake8_options = "-m flake8"
-
--- IndentLine
-vim.g.indentLine_char = "¦"
-
--- CtrlP
-vim.g.ctrlp_map = "<c-p>"
-vim.g.ctrlp_working_path_mode = "c"
-vim.g.ctrlp_use_caching = 0
-
--- Signify
-vim.g.signify_realtime = 1
-
--- Highlight yanks
-vim.g.highlightedyank_highlight_duration = 250
-
--- Startify
-vim.g.startify_disable_at_vimenter = 1
+-- Terminal escape
+map("t", "<Esc>", [[<C-\><C-n>]])
 
 -- ============================================================
--- Tig Command
+-- Custom Command: Tig
 -- ============================================================
+
 vim.api.nvim_create_user_command("Tig", function()
   vim.cmd("below new")
   vim.fn.termopen("tig status")
